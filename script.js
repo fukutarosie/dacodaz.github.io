012d08890812889af63bf7b93fdebe025545590a
desraymondz <desraymondz@gmail.com>
2023-11-03 00:33:03 +0800

    starter code v.1

---
+++ script.js
@@ -0,0 +1,8 @@
+// for real java script code
+
+
+
+
+
+
+// ❤️made with love by dacodaz 13 Nov 2023 Hackathon SIM UOL CSSC 🎓 Desmond, Alex, Vanness, Fukutaro, Cull
---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup () {
+    
+}
+
+function draw () {
+
+}
+
+
+
+
+
+// ❤️made with love by dacodaz 13 Nov 2023 Hackathon SIM UOL CSSC 🎓 Desmond, Alex, Vanness, Fukutaro, Cull
